3d660caacc790c9cfcb63bcb63babae50ac0175c
Orca133 <142914847+Orca133@users.noreply.github.com>
2023-09-04 10:11:34 -0400

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,15 @@
+public class FirstJava {
+        public static void main(String [] args){
+            int first = 10;
+            int second = 11;
+            int sum = first + second;
+            char a = 'k';
+            System.out.println(a);
+            System.out.println("sum is " + sum);
+
+
+
+
+        }
+
+}
